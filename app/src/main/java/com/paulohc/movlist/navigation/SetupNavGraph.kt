cb045e834dc764f650392cbac3599af45f0869fb
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2024-02-04 19:19:44 -0300

    feat: use collectAsStateWithLifecycle

--- app/src/main/java/com/paulohc/movlist/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/paulohc/movlist/navigation/SetupNavGraph.kt
@@ -1,9 +1,9 @@
 package com.paulohc.movlist.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.paulohc.movlist.ui.screens.details.DetailsScreen
@@ -26,7 +26,7 @@ fun SetupNavGraph(
     ) {
         composable(Screen.Home) {
             val viewModel: HomeViewModel = hiltViewModel()
-            val state = viewModel.state.collectAsState().value
+            val state = viewModel.state.collectAsStateWithLifecycle().value
             HomeScreen(
                 state = state,
                 fetchMovies = viewModel::fetchMovies,
@@ -39,7 +39,7 @@ fun SetupNavGraph(
         }
         composable(Screen.Search) {
             val viewModel: SearchViewModel = hiltViewModel()
-            val state = viewModel.state.collectAsState().value
+            val state = viewModel.state.collectAsStateWithLifecycle().value
             SearchScreen(
                 state = state,
                 searchMovies = viewModel::searchMovies,
@@ -52,7 +52,7 @@ fun SetupNavGraph(
         }
         composable(Screen.Details) {
             val viewModel: DetailsViewModel = hiltViewModel()
-            val state = viewModel.state.collectAsState().value
+            val state = viewModel.state.collectAsStateWithLifecycle().value
             DetailsScreen(
                 state = state,
                 navigateBack = navController::popBackStack
