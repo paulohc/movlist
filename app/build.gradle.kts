a1d3ca636f68638037ec4d58fda83a40f3961d34
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2024-02-04 12:12:47 -0300

    build: configure retrofit cache

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.github.skydoves:retrofit-adapters-result:1.0.9")
 
     // Coil
