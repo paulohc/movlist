34378b860e80ca2a6c0f06c4d2236fb16e16fe96
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2024-02-04 18:35:54 -0300

    feat: hide navigation bar for some screens

--- app/src/main/java/com/paulohc/movlist/navigation/CustomNavigationBar.kt
+++ app/src/main/java/com/paulohc/movlist/navigation/CustomNavigationBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -20,31 +19,35 @@ fun CustomNavigationBar(
         CustomNavigationBarItem.Home,
         CustomNavigationBarItem.Search,
     )
-    NavigationBar {
-        items.forEach { item ->
-            val isSelected = currentDestination
-                ?.hierarchy?.any { it.route == item.screen.routePrefix } == true
-            val title = stringResource(id = item.title)
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = if (isSelected) item.selectedIcon
-                        else item.unselectedIcon,
-                        contentDescription = title,
-                    )
-                },
-                label = { Text(title) },
-                selected = isSelected,
-                onClick = {
-                    navController.navigate(item.screen.routePrefix) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+
+    val shouldShowBar = items.any { it.screen.routePrefix == currentDestination?.route }
+
+    if (shouldShowBar) {
+        NavigationBar {
+            items.forEach { item ->
+                val isSelected = currentDestination?.route == item.screen.routePrefix
+                val title = stringResource(id = item.title)
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = if (isSelected) item.selectedIcon
+                            else item.unselectedIcon,
+                            contentDescription = title,
+                        )
+                    },
+                    label = { Text(title) },
+                    selected = isSelected,
+                    onClick = {
+                        navController.navigate(item.screen.routePrefix) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
