efa7722562e9e2770213c4169b98882f399fc08b
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2024-07-19 00:57:40 -0300

    fix: fix search crashes
    Some movies have backdropPath and posterPath nullable values that
    weren't handled.

--- app/src/main/java/com/paulohc/movlist/data/model/Movie.kt
+++ app/src/main/java/com/paulohc/movlist/data/model/Movie.kt
@@ -1,10 +1,10 @@
 package com.paulohc.movlist.data.model
 
 data class Movie(
-    val backdropPath: String,
+    val backdropPath: String?,
     val id: Int,
     val overview: String,
-    val posterPath: String,
+    val posterPath: String?,
     val releaseDate: String,
     val title: String,
     val voteAverage: Double,
--- app/src/main/java/com/paulohc/movlist/data/remote/dto/MovieDto.kt
+++ app/src/main/java/com/paulohc/movlist/data/remote/dto/MovieDto.kt
@@ -8,7 +8,7 @@ data class MovieDto(
     @SerializedName("adult")
     val adult: Boolean,
     @SerializedName("backdrop_path")
-    val backdropPath: String,
+    val backdropPath: String?,
     @SerializedName("genre_ids")
     val genreIds: List<Int>,
     @SerializedName("id")
@@ -22,7 +22,7 @@ data class MovieDto(
     @SerializedName("popularity")
     val popularity: Double,
     @SerializedName("poster_path")
-    val posterPath: String,
+    val posterPath: String?,
     @SerializedName("release_date")
     val releaseDate: String,
     @SerializedName("title")
